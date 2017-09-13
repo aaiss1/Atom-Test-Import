b52f549eb5c03ee474243ab34cd4e43495126b43
Anees Aissaoui <31697581+aaiss1@users.noreply.github.com>
2017-09-13 11:47:37 -0400

    The html and JavaScript files (p5) from Atom have been uploaded.

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() {
+  background(50, 100);
+  stroke(10,200,50);
+  strokeWeight(30);
+  point(mouseX,mouseY);
+  console.log("Check,Check");
+}
